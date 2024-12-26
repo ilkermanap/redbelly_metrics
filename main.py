ede72e56219ac7204dc967d487730f3a7019ef85
Ilker Manap <ilkermanap@gmail.com>
2024-12-27 00:54:32 +0100

    Switched from info to Gauge

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 import time
-from prometheus_client import start_http_server, Info
+from prometheus_client import start_http_server, Gauge
 import subprocess
 
 # Create a metric to track time spent and requests made.
-block_height = Info('node_block_height', 'The latest block of this node')
+block_height = Gauge('node_block_height', 'The latest block of this node')
 
 LOGFILE="/var/log/redbelly/rbn_logs/rbbc_logs.log"
 
 def find_last_block():
     cmd = subprocess.run(["tail", "-n", "20", LOGFILE], capture_output=True)
-    blid = -1
+    blid = None
     for line in cmd.stdout.decode().splitlines():
         if line.find("Imported new chain segment")> -1:
-            blid = int(line.split("{")[1].split(",")[0].split(":")[1].replace('"',''))
+            blid = line.split("{")[1].split(",")[0].split(":")[1].replace('"','')
             return(blid)
                      
         if line.find("with current local block") > -1:
-            blid = int(line.split("with current local block")[1].split()[0])
+            blid = line.split("with current local block")[1].split()[0]
 
-    if blid > -1:
+    if blid is not None:
         return(blid)
 
 def find_last_block_number():
     """A dummy function that takes some time."""
     blid = find_last_block()
     if blid is not None:
-        block_height.info({'height': blid})
+        block_height.set(int(blid))
 
 if __name__ == '__main__':
     # Start up the server to expose the metrics.
