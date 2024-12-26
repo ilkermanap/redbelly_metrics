e2864f80e00c6b8b68e762ccd983d4fd21e055af
Ilker Manap <ilkermanap@gmail.com>
2024-12-27 00:31:29 +0100

    added block height metric

---
+++ main.py
@@ -0,0 +1,36 @@
+import time
+from prometheus_client import start_http_server, Info
+import subprocess
+
+# Create a metric to track time spent and requests made.
+block_height = Info('node_block_height', 'The latest block of this node')
+
+LOGFILE="/var/log/redbelly/rbn_logs/rbbc_logs.log"
+
+def find_last_block():
+    cmd = subprocess.run(["tail", "-n", "20", LOGFILE], capture_output=True)
+    blid = -1
+    for line in cmd.stdout.decode().splitlines():
+        if line.find("Imported new chain segment")> -1:
+            blid = line.split("{")[1].split(",")[0].split(":")[1].replace('"','')
+            return(blid)
+                     
+        if line.find("with current local block") > -1:
+            blid = line.split("with current local block")[1].split()[0]
+
+    if blid > -1:
+        return(blid)
+
+def find_last_block_number():
+    """A dummy function that takes some time."""
+    blid = find_last_block()
+    if blid is not None:
+        block_height.info({'height': blid})
+
+if __name__ == '__main__':
+    # Start up the server to expose the metrics.
+    start_http_server(9101)
+    # Generate some requests.
+    while True:
+        find_last_block_number()
+        time.sleep(30)
